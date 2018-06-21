632fadcef5c2d86cb8ba064070ab3be8679091df
AdityaSrivast <yashsri421@gmail.com>
2018-06-21 23:05:30 +0530

    add responsiveness

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
 	$(".collapsible-body").hide();
 	$(".collapsible-title").click(function(){
-		if($(this).closest('.extensible').length <=0) {
+		if($(this).closest('.extensible').length) {
 			$(".collapsible-body").not($(this).siblings()).slideUp(300);			
 		}
 		$(this).siblings(".collapsible-body").slideToggle(300);
 	})
-	$(".")
 });
